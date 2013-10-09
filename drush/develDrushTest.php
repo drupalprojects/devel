40e37bf564ea4c0f08c41e81c27d351702db2a94
Moshe Weitzman <weitzman@tejasa.com>
2013-10-09 12:39:39 -0400

    A bit more coversage in Drush tests. Commented out for now.

--- drush/develDrushTest.php
+++ drush/develDrushTest.php
@@ -4,10 +4,12 @@
  * @file
  *   PHPUnit Tests for devel. This uses Drush's own test framework, based on PHPUnit.
  *   To run the tests, use run-tests-drush.sh from the devel directory.
+ *
+ *   @todo It is not ideal to download yourself in your own tests.
  */
 class develCase extends Drush_CommandTestCase {
 
-  public function testFnView() {
+  public function testFnCommands() {
     $sites = $this->setUpDrupal(1, TRUE);
     $options = array(
       'root' => $this->webroot(),
@@ -20,5 +22,10 @@ class develCase extends Drush_CommandTestCase {
     $output = $this->getOutput();
     $this->assertContains('@return', $output, 'Output contain @return Doxygen.');
     $this->assertContains('function drush_main() {', $output, 'Output contains function drush_main() declaration');
+
+//    $this->drush('fn-hook', array('cron'), $options);
+//    $output = $this->getOutputAsList();
+//    $expected = array('dblog', 'file', 'field', 'system', 'update');
+//    $this->assertSame($expected, $output);
   }
 }
