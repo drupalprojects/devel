71491becd98abe59917f7c09049d8718d4ae3703
manuelbg <manuelbg@2877829.no-reply.drupal.org>
2014-04-29 13:12:16 +0100

    Issue #2251145 by manuelbg | John_B: Fatal error on devel_generate.

--- devel_generate/lib/Drupal/devel_generate/Plugin/DevelGenerate/ContentDevelGenerate.php
+++ devel_generate/lib/Drupal/devel_generate/Plugin/DevelGenerate/ContentDevelGenerate.php
@@ -55,7 +55,7 @@ class ContentDevelGenerate extends DevelGenerateBase implements ContainerFactory
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, array $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
       $configuration, $plugin_id, $plugin_definition,
       $container->get('field.info')
