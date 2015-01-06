cc910acc703c170e3354acef988b27fb4c9b5f99
Moshe Weitzman <weitzman@tejasa.com>
2015-01-06 10:41:58 -0500

    Don't download devel during a Drush test. Use the local devel.

--- drush/develDrushTest.php
+++ drush/develDrushTest.php
@@ -8,18 +8,20 @@ if (class_exists('Unish\CommandUnishTestCase')) {
    * @file
    *   PHPUnit Tests for devel. This uses Drush's own test framework, based on PHPUnit.
    *   To run the tests, use run-tests-drush.sh from the devel directory.
-   *
-   *   @todo It is not ideal to download yourself in your own tests.
    */
   class develCase extends CommandUnishTestCase {
 
     public function testFnCommands() {
-      $sites = $this->setUpDrupal(1, TRUE);
+      // Specify '8' just in case user has not set UNISH_DRUPAL_MAJOR_VERSION env variable.
+      $sites = $this->setUpDrupal(1, TRUE, '8');
+
+      // Symlink this module into the Site Under test so it can be enabled.
+      $target = dirname(__DIR__);
+      \symlink($target, $this->webroot() . '/modules/devel');
       $options = array(
         'root' => $this->webroot(),
         'uri' => key($sites),
       );
-      $this->drush('pm-download', array('devel'), $options + array('cache' => NULL));
       $this->drush('pm-enable', array('devel'), $options + array('skip' => NULL, 'yes' => NULL));
 
       $this->drush('fn-view', array('drush_main'), $options);
